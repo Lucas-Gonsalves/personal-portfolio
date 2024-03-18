06819432cca3288182e91b49b63e81a575c964af
lucas <lucasluz1710@gmail.com>
2024-03-18 15:52:30 -0300

    feat(routes)  create routes   install react-router-dom create routes with react-router-dom and incremented the page home.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "styled-components";
 
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/theme/default";
+import { Routes } from "./routes";
 
 
 export function App() {
@@ -10,6 +11,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle/>
+      <Routes/>
     </ThemeProvider>
   );
 };
